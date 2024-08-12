e1c177f4e3ce532c6ceb41fb996df90c94e488e0
Kostenko Oleg <ok609035@gmail.com>
2024-08-12 18:46:21 +0300

    /

--- src/components/TransactionForm/TransactionList.jsx
+++ src/components/TransactionForm/TransactionList.jsx
@@ -15,7 +15,7 @@ export default function TransactionList({ transactions }) {
           <p className={styles.name}>{transaction.name}</p>
           <span> {formatDistanceToNow(new Date(transaction.createdAt.toDate()), { addSuffix: true, locale: ru })}</span>
           </div>
-          <p className={styles.amount}>${transaction.amount}</p>
+          <p className={styles.amount}>{transaction.amount}$</p>
           <button onClick={() => deleteDocument(transaction.id)}>x</button>
         </li>
       ))}
